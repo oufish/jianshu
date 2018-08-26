098004bed42e0ba940be0c87bb6e6431153a08db
oufish <674106992@qq.com>
2018-08-26 16:50:33 +0800

    顶部基础布局

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css';
-import './index.css';
+import './common.js';
+import './statics/iconfont/iconfont';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
